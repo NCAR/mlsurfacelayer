4d99b021d3906c410783b093fd18468796ae02b3
David John Gagne <djgagne@ou.edu>
2018-10-03 15:53:29 -0600

    Added common variables to data processing.

--- mlsurfacelayer/data.py
+++ mlsurfacelayer/data.py
@@ -2,21 +2,27 @@ import pandas as pd
 from glob import glob
 from os.path import join
 from .derived import *
+from pvlib.solarposition import get_solarposition
 
 
-def process_cabauw_data(csv_path, out_file, nan_column=("soil", "TS00")):
+def process_cabauw_data(csv_path, out_file, nan_column=("soil_water", "TH03")):
     """
     This function loads all of the cabauw data files and then calculates the relevant derived quantities necessary
     to build the machine learning parameterization.
 
     Args:
         csv_path: Path to all csv files.
+        out_file: Where derived data are written to.
+        nan_column: Column used to filter bad examples.
 
     Returns:
         `pandas.DataFrame` containing derived data.
     """
+    cabauw_lat = 51.971
+    cabauw_lon = 4.926
+    elevation = -0.7
     csv_files = sorted(glob(join(csv_path, "*.csv")))
-    file_types = [csv_file.split("/")[-1].split("_")[1] for csv_file in csv_files]
+    file_types = ["_".join(csv_file.split("/")[-1].split("_")[1:-1]) for csv_file in csv_files]
     data = dict()
     for c, csv_file in enumerate(csv_files):
         print(csv_file)
@@ -25,20 +31,41 @@ def process_cabauw_data(csv_path, out_file, nan_column=("soil", "TS00")):
     print("combine data")
     combined_data = pd.concat(data, axis=1, join="inner")
     combined_data = combined_data.loc[~pd.isna(combined_data[nan_column])]
-    derived_columns = ["temperature_10 m_K",
+    derived_columns = ["global horizontal irradiance_0 m_W m-2",
+                       "zenith_0 m_degrees",
+                       "azimuth_0 m_degrees",
+                       "temperature_10 m_K",
+                       "temperature_2 m_K",
                        "pressure_2 m_hPa",
                        "potential temperature_10 m_K",
+                       "potential temperature_2 m_K",
                        "mixing ratio_10 m_g kg-1",
+                       "relative humidity_10 m_%",
                        "virtual potential temperature_10 m_K",
                        "air density_10 m_kg m-3",
                        "wind speed_10 m_m s-1",
                        "wind direction_10 m_m s-1",
+                       "wind speed_20 m_m s-1",
+                       "wind direction_20 m_m s-1",
+                       "wind speed_40 m_m s-1",
+                       "wind direction_40 m_m s-1",
                        "u wind_10 m_m s-1",
                        "v wind_10 m_m s-1",
+                       "u wind_20 m_m s-1",
+                       "v wind_20 m_m s-1",
+                       "u wind_40 m_m s-1",
+                       "v wind_40 m_m s-1",
                        "mixing ratio_2 m_g kg-1",
-                       "temperature_0 m_K",
-                       "potential temperature_0 m_K",
-                       "virtual potential temperature_0 m_K",
+                       "virtual potential temperature_10 m_K",
+                       "relative humidity_2 m_%",
+                       "soil temperature_0 cm_K",
+                       "soil temperature_4 cm_K",
+                       "soil temperature_6 cm_K",
+                       "soil potential temperature_0 cm_K",
+                       "soil potential temperature_4 cm_K",
+                       "soil potential temperature_6 cm_K",
+                       "soil water content_3 cm_m3 m-3",
+                       "soil water content_8 cm_m3 m-3",
                        "friction velocity_surface_K",
                        "temperature scale_surface_K",
                        "moisture scale_surface_g kg-1",
@@ -46,28 +73,46 @@ def process_cabauw_data(csv_path, out_file, nan_column=("soil", "TS00")):
                        "obukhov length_surface_m"
                        ]
     derived_data = pd.DataFrame(index=combined_data.index, columns=derived_columns, dtype=float)
+    solar_data = get_solarposition(combined_data.index, cabauw_lat, cabauw_lon, altitude=elevation, method="nrel_numba")
     print("calculate derived variables")
+    derived_data["global horizontal irradiance_0 m_W m-2"] = combined_data[("surface", "SWD")]
+    derived_data["zenith_0 m_degrees"] = solar_data["zenith"]
+    derived_data["azimuth_0 m_degrees"] = solar_data["azimuth"]
     derived_data["temperature_10 m_K"] = combined_data[("tower", "TA_10m")]
+    derived_data["temperature_2 m_K"] = combined_data[("tower", "TA_2m")]
     derived_data["pressure_2 m_hPa"] = combined_data[("surface", "P0")]
     derived_data["potential temperature_10 m_K"] = potential_temperature(derived_data["temperature_10 m_K"],
                                                                          derived_data["pressure_2 m_hPa"])
+    derived_data["potential temperature_2 m_K"] = potential_temperature(derived_data["temperature_2 m_K"],
+                                                                         derived_data["pressure_2 m_hPa"])
     derived_data["mixing ratio_10 m_g kg-1"] = combined_data[("tower", "Q_10m")]
+    derived_data["relative humidity_10 m_%"] = combined_data[("tower", "RH_10m")]
+    derived_data["relative humidity_2 m_%"] = combined_data[("tower", "RH_2m")]
     derived_data["virtual potential temperature_10 m_K"] = virtual_temperature(
         derived_data["potential temperature_10 m_K"], derived_data["mixing ratio_10 m_g kg-1"])
+    derived_data["virtual potential temperature_2 m_K"] = virtual_temperature(
+        derived_data["potential temperature_2 m_K"], derived_data["mixing ratio_2 m_g kg-1"])
     derived_data["air density_10 m_kg m-3"] = air_density(virtual_temperature(derived_data["temperature_10 m_K"],
                                                                               derived_data["mixing ratio_10 m_g kg-1"]),
                                                           derived_data["pressure_2 m_hPa"])
-    derived_data["wind speed_10 m_m s-1"] = combined_data[("tower", "F_10m")]
-    derived_data["wind direction_10 m_m s-1"] = combined_data[("tower", "D_10m")]
-    derived_data["u wind_10 m_m s-1"], \
-    derived_data["v wind_10 m_m s-1"] = wind_components(derived_data["wind speed_10 m_m s-1"],
-                                                        derived_data["wind direction_10 m_m s-1"])
+    for height in [10, 20, 40]:
+        derived_data["wind speed_{0:d} m_m s-1".format(height)] = combined_data[("tower", "F_{0:d}m".format(height))]
+        derived_data["wind direction_{0:d} m_m s-1".format(height)] = combined_data[("tower", "D_{0:d}m".format(height))]
+        derived_data["u wind_{0:d} m_m s-1".format(height)], derived_data["v wind_{0:d} m_m s-1".format(height)] = \
+            wind_components(derived_data["wind speed_{0:d} m_m s-1".format(height)],
+                            derived_data["wind direction_{0:d} m_m s-1".format(height)])
     derived_data["mixing ratio_2 m_g kg-1"] = combined_data[("tower", "Q_2m")]
-    derived_data["temperature_0 m_K"] = celsius_to_kelvin(combined_data[("soil", "TS00")])
-    derived_data["potential temperature_0 m_K"] = potential_temperature(derived_data["temperature_0 m_K"],
-                                                                        derived_data["pressure_2 m_hPa"])
-    derived_data["virtual potential temperature_0 m_K"] = virtual_temperature(derived_data["potential temperature_0 m_K"],
-                                                                              derived_data["mixing ratio_2 m_g kg-1"])
+    derived_data["soil temperature_0 cm_K"] = celsius_to_kelvin(combined_data[("soil", "TS00")])
+    derived_data["soil temperature_4 cm_K"] = celsius_to_kelvin(combined_data[("soil", "TS04")])
+    derived_data["soil temperature_6 cm_K"] = celsius_to_kelvin(combined_data[("soil", "TS06")])
+    derived_data["soil potential temperature_0 m_K"] = potential_temperature(derived_data["soil temperature_0 cm_K"],
+                                                                             derived_data["pressure_2 m_hPa"])
+    derived_data["soil potential temperature_0 m_K"] = potential_temperature(derived_data["soil temperature_4 cm_K"],
+                                                                             derived_data["pressure_2 m_hPa"])
+    derived_data["soil potential temperature_0 m_K"] = potential_temperature(derived_data["soil temperature_6 cm_K"],
+                                                                             derived_data["pressure_2 m_hPa"])
+    derived_data["soil water content_3 cm_m3 m-3"] = combined_data[("soil_water", "TH03")]
+    derived_data["soil water content_8 cm_m3 m-3"] = combined_data[("soil_water", "TH08")]
     derived_data["friction velocity_surface_m s-1"] = combined_data[("flux", "UST")]
     derived_data["temperature scale_surface_K"] = temperature_scale(combined_data[("flux", "H")],
                                                                     derived_data["air density_10 m_kg m-3"],
@@ -75,11 +120,16 @@ def process_cabauw_data(csv_path, out_file, nan_column=("soil", "TS00")):
     derived_data["moisture scale_surface_g kg-1"] = moisture_scale(combined_data[("flux", "LE")],
                                                                    derived_data["air density_10 m_kg m-3"],
                                                                    derived_data["friction velocity_surface_K"])
-    derived_data["bulk richardson_surface_"] = bulk_richardson_number(derived_data["potential temperature_10 m_K"],
-                                                                      10,
-                                                                      derived_data["mixing ratio_10 m_g kg-1"],
-                                                                      derived_data["virtual potential temperature_0 m_K"],
-                                                                      derived_data["wind speed_10 m_m s-1"])
+    derived_data["bulk richardson_10 m_"] = bulk_richardson_number(derived_data["potential temperature_10 m_K"],
+                                                                   10,
+                                                                   derived_data["mixing ratio_10 m_g kg-1"],
+                                                                   derived_data["soil potential temperature_4 cm_K"],
+                                                                   derived_data["wind speed_10 m_m s-1"])
+    derived_data["bulk richardson_2 m_"] = bulk_richardson_number(derived_data["potential temperature_2 m_K"],
+                                                                  2,
+                                                                  derived_data["mixing ratio_2 m_g kg-1"],
+                                                                  derived_data["soil potential temperature_4 cm_K"],
+                                                                  derived_data["wind speed_10 m_m s-1"])
     derived_data["obukhov length_surface_m"] = obukhov_length(derived_data["potential temperature_10 m_K"],
                                                               derived_data["temperature scale_surface_K"],
                                                               derived_data["friction velocity_surface_K"])
---
+++ scripts/MoninObukhov.py
@@ -0,0 +1,223 @@
+
+"""
+This code computes surface friction velocity and surface potential 
+temperature scale using Monin-Obukhov Similarity Theory and measurements of
+surface temperature as well as potential temperature and wind speed at 
+some reference level z1
+
+Written by Branko Kosovic, September 21, 2018
+"""
+import numpy as np
+import scipy
+from math import *
+import sys
+import matplotlib.pyplot as plt
+
+
+def mo_similarity(u10,v10,tsk,t2,qsfc,q2,psfc,z0,zt0):
+    # u10 - 10 m level zonal wind speed [m/s]
+    # v10 - 10 m level meridional wind speed [m/s]
+    # tsk - surface skin temperature [K]
+    # t2  - 2 m level temperature [K]
+    # qsfc - ground mixing ratio
+    # q2  - 2 m level mixing ratio
+    # psfc - surface pressure
+    # z0 - momentum roughness length
+    # zt0 - heat flux roughness length
+    #
+    # Levels z10 and z2 are set to 10 m and 2m, 
+    # but they can be any two levels
+    z10  = 10.
+    z2   = 2.0
+    if (z0 > 0.):
+        z10oz0 = z10/z0
+        z2oz0  = z2/z0
+        z2ozt0 = z2/zt0
+    else:
+        sys.exit("Surface roughnes, z0, must be greter than 0.!")
+    #
+    # Gravitational acceleration
+    g = 9.81
+    #
+    # Gas constant over spcific heat capacity at constant pressure
+    r = 287.058
+    cp = 1005.
+    rocp = r/cp
+    #
+    # Reference pressure and temperature
+    p0  = 1000.
+    t0  = 300.
+    #
+    # Set M-O parameters based on Dyer 1974 paper
+    karman = 0.4
+    beta   = 5.0
+    gamma  = 16.0
+    #
+    # Air density
+    rho = psfc/(r*t2)
+    #
+    # Potential temperature speed at level 1
+    th2 = t2*(p0/psfc)**rocp
+    th0 = tsk*(p0/psfc)**rocp
+    #
+    # Virtual potential temperature
+    thv2 = th2*(1.+0.61*q2)
+    thv0 = th0*(1.+0.61*qsfc)
+    #
+    # Small number
+    epsilon = 1.e-6
+    #
+    # Initial values of drag coefficients - neutrally stratified case
+    cd = karman**2/((log(z10oz0))**2)
+    ch = karman**2/((log(z2ozt0))**2)
+    cq = karman**2/((log(z2ozt0))**2)
+    # 
+    # Initial values of surface friction velocity, temperature scale, and
+    # heat flux
+    tauxz = cd*sqrt(u10*u10+v10*v10)*u10
+    tauyz = cd*sqrt(u10*u10+v10*v10)*v10
+    ustar = (tauxz**2+tauyz**2)**0.25
+    tstar = ch/ustar*sqrt(u10*u10+v10*v10)*(th0-th2)
+    wthv0 = ustar*tstar
+    #
+    # Set stopping criterion
+    diff = 1.
+    # 
+    # Set stability functions
+    psim10 = 0.
+    psim2  = 0.
+    psih2  = 0.
+    psiq2  = 0.
+    phim10 = 0.
+    phih2 = 0
+    #
+    while (diff > epsilon):
+        #
+        # Surface friction velocity and temperature scale
+        tauxz = cd*sqrt(u10*u10+v10*v10)*u10
+        tauyz = cd*sqrt(u10*u10+v10*v10)*v10
+        ustar = (tauxz**2+tauyz**2)**0.25
+        wspd2 = ustar/karman*(log(z2oz0)-psim2)
+        tstar = ch/ustar*sqrt(u10*u10+v10*v10)*(thv0-thv2)
+        wthv0 = ustar*tstar
+        #
+        # Compute drag coefficients
+        cdold = cd
+        chold = ch
+        cqold = cq
+        #
+        # Neutrally stratified case
+        if (wthv0 == 0.): 
+            zeta10 = 0.
+            zeta2  = 0.
+            psim10 = 0.
+            psim2  = 0.
+            psih2  = 0.
+            psiq2  = 0.
+            phim10 = 1.
+            phih2  = 1.
+            cd = karman**2/((log(z10oz0))**2)
+            ch = karman**2/((log(z2ozt0))**2)
+            cq = karman**2/((log(z2ozt0))**2)
+        #
+        if (abs(wthv0) > 0.):
+            #
+            # Obukhov length scale 
+            olength = -ustar**3/(karman*g/t0*wthv0)
+            #
+            # Free convection
+            if (olength == 0.):
+                sys.exit("Free convection!")
+            #
+            # Monin-Obukhov stability parameter
+            zeta10 = z10/olength
+            zeta2 = z2/olength
+            #
+            # Convective case
+            if ((zeta2  < -epsilon) & (zeta10 >= -2.)):
+                xi10 = 1./((1.-gamma*zeta10)**0.25)
+                xi2  = 1./((1.-gamma*zeta2)**0.25)
+                psim10 = log(0.5*(1.0+xi10**2)*(0.5*(1.0+xi10))**2) \
+                       -2.*atan(xi10)+0.5*np.pi 
+                psim2  = log(0.5*(1.0+xi2**2)*(0.5*(1.0+xi2))**2) \
+                        -2.*atan(xi2)+0.5*np.pi 
+                psih2  = 2.0*log(0.5*(1.0+xi2**2))
+                psiq2  = 2.0*log(0.5*(1.0+xi2**2))
+                phim10 =1./((1.-gamma*zeta10)**0.25)
+                phih2  =1./((1.-gamma*zeta2)**0.25)
+            #
+            # Stably stratified case
+            if ((zeta2  > +epsilon) & (zeta10 <= 1.)):
+                psim10 = - beta*zeta10
+                psim2 = - beta*zeta2
+                psih2 = - beta*zeta2
+                psiq2 = - beta*zeta2
+                phim10 =(1.+beta*zeta10) 
+                phih2  =(1.+beta*zeta2) 
+            #
+            # Neutrally stratified case
+            if ((zeta2  <= +epsilon) & (zeta2 >= -epsilon)):
+                psim10 = 0.
+                psim2  = 0.
+                psih2  = 0.
+                psiq2  = 0.
+                phim10 = 1.
+                phih2  = 1.
+            #
+            cd = karman**2/((log(z10oz0)-psim10)**2)
+            ch = karman**2/((log(z2ozt0)-psim2)*(log(z2ozt0)-psih2))
+            cq = karman**2/((log(z2ozt0)-psim2)*(log(z2ozt0)-psiq2))
+        #
+        diff = abs(cd-cdold)+abs(ch-chold)+abs(cq-cqold)
+    #
+    return ustar, tstar, wthv0, zeta10, phim10, zeta2, phih2
+#
+########################
+#
+#--- Inputs
+# Wind velocity at 10 m
+u10 = 5.
+v10 = 0.
+#
+# Skin temperature and temperature at 2 m
+tsk = 280.
+t2  = 290.
+#
+# Ground mixing ratio and mixing ration at 2 m
+qsfc = 0.01
+q2   = 0.01
+#
+# Surface pressure 
+psfc = 1005.
+#
+# Momentum roughness length, heat flux roughness and reference level
+z0  = 0.1
+zt0 = z0
+#
+phim=[]
+zolm=[]
+phih=[]
+zolh=[]
+#
+for tsk in np.arange(289.,300.,0.01):
+    ustar,tstar,wthv0,zeta10,phim10,zeta2,phih2 = \
+                       mo_similarity(u10,v10,tsk,t2,qsfc,q2,psfc,z0,zt0)
+    if ((zeta10 < -2.) | (zeta10 > 1.)):
+        print(" zeta10 = ",zeta10)
+        #sys.exit("Stability parameter z/L outside the range of validiti of Monin-Obukhov similarity theory!")
+    phim.append(phim10)
+    zolm.append(zeta10)
+    phih.append(phih2)
+    zolh.append(zeta2)
+    #
+    print("ustar = ",ustar,"  tstar = ",tstar," wthv0 = ",wthv0)
+#
+p=plt.plot(zolm,phim,'ro')
+plt.xlabel("z/L")
+plt.ylabel("$\Phi_m$")
+plt.show()
+q=plt.plot(zolh,phih,'ro')
+plt.xlabel("z/L")
+plt.ylabel("$\Phi_h$")
+plt.show()
+
