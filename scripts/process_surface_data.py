3a5d080254f31093766daa6e3a137d1b2b120882
David John Gagne <djgagne@ou.edu>
2019-12-11 15:02:56 -0800

    Updated code and config files for resampling.

--- scripts/process_surface_data.py
+++ scripts/process_surface_data.py
@@ -8,9 +8,10 @@ def main():
     parser.add_argument("-s", "--site", default="cabauw", help="Site type: cabauw, nrel, idaho")
     parser.add_argument("-o", "--output", help="Output file.")
     parser.add_argument("-r", "--refl", action="store_true", help="Change sign of Counter Gradient Fluxes")
+    parser.add_argument("-w", "--wind", default="30Min", help="Averaging window")
     args = parser.parse_args()
     if args.site == "cabauw":
-        process_cabauw_data(args.input, args.output, reflect_counter_gradient=args.refl)
+        process_cabauw_data(args.input, args.output, reflect_counter_gradient=args.refl, average_period=args.wind)
 
     return
 
