3301b65f2d57ab0942fd26bb7e0999201f6adece
David John Gagne <djgagne@ou.edu>
2020-03-23 17:13:14 -0600

    New fortran neural net and random forest modules

---
+++ mlsurfacelayer/compile_neural_net.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DEBUG_FLAGS="-fPIC -g -fimplicit-none  -Wall  -Wline-truncation  -Wcharacter-truncation  -Wsurprising  -Waliasing  -Wimplicit-interface  -Wunused-parameter  -fwhole-file  -fcheck=all  -std=f2008  -pedantic  -fbacktrace -fbounds-check -ffpe-trap=zero,invalid,overflow,underflow"
+NC_FLAGS="-I/opt/local/include/ -L/opt/local/lib -lnetcdff"
+rm *.mod *.o
+gfortran $DEBUG_FLAGS -c module_neural_net.f90 $NC_FLAGS
+gfortran $DEBUG_FLAGS -c module_sf_sfclay_neural_net.f90 *.o $NC_FLAGS
+gfortran $DEBUG_FLAGS test_neural_net.f90 *.o -o test_neural_net $NC_FLAGS 
--- mlsurfacelayer/run_random_forest.f90
+++ mlsurfacelayer/run_random_forest.f90
@@ -1,24 +1,25 @@
 program run_random_forest
-    use random_forest
+    use module_random_forest
     implicit none
     character(len=200) :: file_path
     type(decision_tree), allocatable :: rf(:)
-    real(kind=8) :: input_data(15)
-    real(kind=8) :: prediction
+    real(kind=8) :: input_data(1, 15)
+    real(kind=8) :: prediction(1)
     integer :: n, num_trees
-    file_path = "/Users/dgagne/mlsurfacelayer/qstar_rf/"
+    file_path = "/Users/dgagne/mlsurfacelayer/ustar_rf/"
+    print*, file_path
     call load_random_forest(file_path, rf)
     open(345, file="test_input.csv")
     read(345, "(A)")
     read(345, *) input_data
     close(345)
     print *, input_data
-    print *, size(input_data)
-    print *, size(rf)
+    print *, "Input Size", size(input_data, 1), size(input_data, 2)
+    print *, "RF Size ", size(rf)
     num_trees = size(rf)
     do n=1,num_trees
         print*, rf(n)%feature(1), rf(n)%impurity(1)
     end do
-    prediction = random_forest_predict(input_data, rf)
+    call random_forest_predict(input_data, rf, prediction)
     print *, prediction
 end program run_random_forest
