a0d019dcf786e74dd5b6ca85e91e2268581709e6
David John Gagne <davidjohngagne@gmail.com>
2020-04-29 08:40:33 -0600

    Fixed data processing and added small variable set experiments.

--- mlsurfacelayer/compile_neural_net.sh
+++ mlsurfacelayer/compile_neural_net.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DEBUG_FLAGS="-fPIC -g -fimplicit-none  -Wall  -Wline-truncation  -Wcharacter-truncation  -Wsurprising  -Waliasing  -Wimplicit-interface  -Wunused-parameter  -fwhole-file  -fcheck=all  -std=f2008  -pedantic  -fbacktrace -fbounds-check -ffpe-trap=zero,invalid,overflow,underflow"
-NC_FLAGS="-I/opt/local/include/ -L/opt/local/lib -lnetcdff"
+DEBUG_FLAGS="-fPIC -g -fimplicit-none  -Wall  -O3 -Wline-truncation  -Wcharacter-truncation  -Wsurprising  -Waliasing  -Wimplicit-interface  -Wunused-parameter  -fwhole-file  -fcheck=all  -std=f2008  -pedantic  -fbacktrace -fbounds-check -ffpe-trap=zero,invalid,overflow,underflow"
+NC_FLAGS="-I/opt/local/include/ -L/opt/local/lib -lnetcdff -llapack -lblas"
 rm *.mod *.o
 gfortran $DEBUG_FLAGS -c module_neural_net.f90 $NC_FLAGS
 gfortran $DEBUG_FLAGS -c module_sf_sfclay_neural_net.f90 *.o $NC_FLAGS
