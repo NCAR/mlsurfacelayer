627949bfdb95a8cc08348e7bf5f0104a7687b452
Susan Dettling <dettling@vpn70-31.vpnclient.ucar.edu>
2020-08-24 13:48:45 -0600

    updating -

--- scripts/process_surface_data.py
+++ scripts/process_surface_data.py
@@ -2,11 +2,12 @@
 import argparse
 from mlsurfacelayer.data import process_cabauw_data
 from mlsurfacelayer.data import process_idaho_data
+from mlsurfacelayer.fino_data import process_fino_data
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="Input data path.")
-    parser.add_argument("-s", "--site", default="cabauw", help="Site type: cabauw, idaho")
+    parser.add_argument("-s", "--site", default="cabauw", help="Site type: cabauw, idaho, fino")
     parser.add_argument("-o", "--output", help="Output file.")
     parser.add_argument("-r", "--refl", action="store_true", help="Change sign of Counter Gradient Fluxes")
     parser.add_argument("-w", "--wind", default="30Min", help="Time-averaging window in pandas time units. 30Min is the default.")
@@ -15,6 +16,8 @@ def main():
         process_cabauw_data(args.input, args.output, reflect_counter_gradient=args.refl, average_period=args.wind)
     if args.site == "idaho":
         process_idaho_data(args.input, args.output, average_period=args.wind)
+    if args.site == "fino":
+        process_fino_data(args.input, args.output, reflect_counter_gradient=args.refl)
     return
 
 if __name__ == "__main__":
