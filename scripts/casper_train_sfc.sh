60ec2236071142b288a024c89b86347679dea9a3
djgagne@ou.edu <davidjohngagne@gmail.com>
2021-03-08 15:27:51 -0700

    Updated config files with fasteddy configs

--- scripts/casper_train_sfc.sh
+++ scripts/casper_train_sfc.sh
@@ -6,6 +6,7 @@
 #SBATCH --partition=dav
 #SBATCH --mem=128G
 #SBATCH --cpus-per-task=1
+#SBATCH --output=sfc_layer_train.out.%j
 export PATH="$HOME/miniconda3/envs/sfc/bin:$PATH"
 cd ~/mlsurfacelayer/scripts/
 python -u train_surface_models.py ../config/surface_layer_training_fasteddy_cabauw_20201203.yml 
